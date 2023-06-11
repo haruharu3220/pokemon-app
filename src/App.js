db543ffe6638375b63ae42aeef199fd2821893ad
Hayato <hayato.314.nico@gmail.com>
2023-06-12 07:33:21 +0900

    API修正

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { useEffect } from "react";
 import "./App.css";
+import { getAllPokemon } from "./utils/pokemon";
 
 function App() {
   const initialURL = "https://pokeapi.co/api/v2/pokemon/1";
 
   useEffect(() => {
     const fetchPokemonData = async () => {
-      let res = await getAiiPokemon(initialURL);
+      let res = await getAllPokemon(initialURL);
+      console.log(res);
     };
+    getAllPokemon();
   }, []);
   return <div className="App"></div>;
 }
